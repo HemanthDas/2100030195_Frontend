b26df43ca6e3d97c955a84e0ecfd31279b738fae
HemanthDas <hemanthchoudary4@gmail.com>
2024-04-30 00:01:30 +0530

    Add button Update UI

--- src/assets/employees.js
+++ src/assets/employees.js
@@ -1,21 +1,21 @@
 const employees = [
   {
     id: 1,
-    FirstName: "Sam",
-    LastName: "Adam",
-    Gender: "Male",
-    Age: 45,
-    Salary: 100000.0,
-    Department: "IT",
+    firstName: "Sam",
+    lastName: "Adam",
+    gender: "Male",
+    age: 45,
+    salary: 100000.0,
+    department: "IT",
   },
   {
     id: 2,
-    FirstName: "John",
-    LastName: "Jacob",
-    Gender: "Male",
-    Age: 35,
-    Salary: 90000.0,
-    Department: "Sales",
+    firstName: "John",
+    lastName: "Jacob",
+    gender: "Male",
+    age: 35,
+    salary: 90000.0,
+    department: "Sales",
   },
 ];
 export default employees;
--- src/components/addItem.jsx
+++ src/components/addItem.jsx
@@ -1,10 +1,13 @@
 import data from "../assets/employees";
 import PropTypes from "prop-types";
+import { useNavigate } from "@tanstack/react-router";
 const AddItem = ({ newData }) => {
+  const navigate = useNavigate();
   const onAdd = () => {
     console.log(newData);
     data.push(newData);
     console.log(data);
+    navigate({ to: "/employee" });
   };
 
   return <button onClick={onAdd}>ADD</button>;
